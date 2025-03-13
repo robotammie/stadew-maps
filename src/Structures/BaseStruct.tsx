467a059b47b25aaa1a973fd722d960e9c9fe8dfc
Tammie Kahnhauser <tammie.kahnhauser@gmail.com>
2025-03-13 16:25:10 -0700

    Update dependencies

---
+++ src/Structures/BaseStruct.tsx
@@ -0,0 +1,13 @@
+import {  StructProps } from '../types.d'
+
+export const styles = (props: StructProps) => {
+  return {
+    margin: `0px ${props.onMap? 0 : 10}px`,
+    padding: 0,
+    cursor: 'pointer',
+    width: '20px',
+    height: '20px',
+    border: (props.onMap ? 'none' : '1px solid black'),
+    backgroundColor: props.bgColor || 'tan',
+  }
+};
